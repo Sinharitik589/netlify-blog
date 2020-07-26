6a3b682aecc4108c244fa9fef99edac295d67c01
Sinharitik589 <sinharitik18112835@gmail.com>
2020-07-26 23:35:57 +0530

    removed cors

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const Serverless = require("serverless-http");
-const cors = require("cors");
+
 const app = express();
-app.use(cors);
 
 const router = express.Router();
 
