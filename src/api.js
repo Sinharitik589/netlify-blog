455204cd54d925de2040bcd7a228db51e0b879ee
Sinharitik589 <sinharitik18112835@gmail.com>
2020-07-26 22:45:02 +0530

    added json

--- src/api.js
+++ src/api.js
@@ -6,7 +6,9 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("hello");
+  res.json({
+    hello: "hi",
+  });
 });
 
 app.use("/.netlify/functions/api", router);
