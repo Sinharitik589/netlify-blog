4b46b73e5f219674126cfeee04aed0a707dc1dc5
Sinharitik589 <sinharitik18112835@gmail.com>
2020-08-02 10:14:05 +0530

    api changed

--- src/api.js
+++ src/api.js
@@ -11,9 +11,26 @@ app.use(function (req, res, next) {
 });
 
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi",
-  });
+  res.json(
+    {
+      title: "Title1",
+      name: "Movie1",
+      disclaimer:
+        "Do anim laboris cillum consectetur sit. Qui Lorem tempor duis aliqua ex dolor officia amet elit dolore veniam. Labore magna commodo enim irure in eiusmod veniam tempor magna. Veniam velit amet dolor voluptate cillum id laborum nisi tempor.",
+    },
+    {
+      title: "Title2",
+      name: "Movie2",
+      disclaimer:
+        "Do anim laboris cillum consectetur sit. Qui Lorem tempor duis aliqua ex dolor officia amet elit dolore veniam. Labore magna commodo enim irure in eiusmod veniam tempor magna. Veniam velit amet dolor voluptate cillum id laborum nisi tempor.",
+    },
+    {
+      title: "Title3",
+      name: "Movie3",
+      disclaimer:
+        "Do anim laboris cillum consectetur sit. Qui Lorem tempor duis aliqua ex dolor officia amet elit dolore veniam. Labore magna commodo enim irure in eiusmod veniam tempor magna. Veniam velit amet dolor voluptate cillum id laborum nisi tempor.",
+    }
+  );
 });
 
 app.use("/.netlify/functions/api", router);
