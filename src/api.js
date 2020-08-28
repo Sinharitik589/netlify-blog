a436fcd70a4c20f358c4650f2d06c056ac48571c
Sinharitik589 <sinharitik18112835@gmail.com>
2020-08-28 17:20:02 +0530

    cors added multiple origin

--- src/api.js
+++ src/api.js
@@ -15,7 +15,19 @@ mongoose.connect(uri, { useNewUrlParser: true }).catch((error) => {
 const router = express.Router();
 require("./mongo/index");
 require("./routes/index")(router, app);
-app.use(cors({ origin: "http://127.0.0.1:5500" }));
+
+var whitelist = ["http://127.0.0.1:5500", "https://sinharitik589.github.io"];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
 app.use("/.netlify/functions/api", router);
 
